e71f0b45623b7202be501273c5f7d8d23bffc234
ibianche <inna.pomorska@gmail.com>
2021-10-07 20:53:18 +0200

    const activeProduct

--- dist/js/script.js
+++ dist/js/script.js
@@ -101,7 +101,7 @@
 
 
         /* find active product (product that has active class) */
-        const activeProduct = document.querySelector(clickableTrigger);
+        const activeProduct = document.querySelector(select.all.menuProductsActive);
 
 
         /* if there is active product and it's not thisProduct.element, remove class active from it */
