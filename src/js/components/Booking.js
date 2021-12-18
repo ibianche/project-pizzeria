0153c54a40331cfb5ed8403f361d30e83df9e389
ibianche <inna.pomorska@gmail.com>
2021-12-18 21:20:54 +0100

    class Booking

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,10 +1,53 @@
-
+import {utils} from "../utils";
+import {select, classNames, templates} from '../settings.js';
+import AmountWidget from './AmountWidget.js';
 
 class Booking {
+  constructor(element) {
+    const thisBooking = this;
+
+    thisBooking.render();
+    thisBooking.initWidgets();
+  }
+
+  render(element) {
+    const thisBooking = this;
+    //generuję kod za pomocą szablonu templates.bookingWidget
+    const generatedHTML = templates.bookingWidget(element);
+
+    //tworzę pusty obiekt
+    thisBooking.dom = {};
+
+    //dodaję do pustego obiektu własciwość wrapper i przupisuję referencję do kontenera
+    thisBooking.dom.wrapper(element);
+
+    //zmiana wartości wrappera innerHTML na kod wygenerowany z szablonu
+    thisBooking.dom.wrapper.innerHTML = generatedHTML;
+
+
+    thisBooking.dom.peopleAmount = document.querySelector(select.widgets.booking.peopleAmount);
+    thisBooking.dom.hoursAmount = document.querySelector(select.widgets.booking.hoursAmount);
+
+  }
+
+  initWidgets() {
+    const thisBooking = this;
+    //tworzę nową instancje AmountWidget
+    thisBooking.peopleAmount = new AmountWidget(thisBooking.dom.peopleAmount);
+    thisBooking.dom.peopleAmount.addEventListener('click', function () {
+
+    });
+    //tworzę nową instancje AmountWidget
+    thisBooking.hoursAmount = new AmountWidget(thisBooking.dom.hoursAmount);
+    thisBooking.dom.hoursAmount.addEventListener('click', function () {
+
+    });
+
+
+  }
 
 
 }
-
 
 
 export default Booking;
